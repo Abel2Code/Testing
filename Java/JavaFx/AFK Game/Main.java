4bea15366702ac224e5a04fedb62460048d72f51
Abel2Code <asalinaswc1@aol.com>
2017-04-11 09:53:53 -0700

    AFK Game now functional

--- Java/JavaFx/AFK Game/Main.java
+++ Java/JavaFx/AFK Game/Main.java
@@ -3,38 +3,34 @@ package application;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	private int counter = 0;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		/*primaryStage.setTitle("AFK Game");
-		final Text HEADER = new Text("Welcome to this AFK game!");
-		StackPane root = new StackPane();
-		root.getChildren().add(HEADER);
-		Scene sc = new Scene(root, 400, 400);
-		primaryStage.setScene(sc);
-		primaryStage.show();	
-
-		BorderPane root = new BorderPane();			
-		primaryStage.setTitle("AFK Game");
-		final Text TITLE = new Text("Welcome to this AFK game!");
-		root.getChildren().add(TITLE);
-		Scene scene = new Scene(root,400,400);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();*/
 		
 		VBox window = new VBox();
 		Label header = new Label("Welcome to this AFK game!");
 		Scene sc = new Scene(window, 300, 300);
+		Label score = new Label("Score: " + counter);
+		// int counter = 0;
+		Button pressMe = new Button("Press Me!");
+		pressMe.setOnMouseClicked(e ->{
+			score.setText("Score: " + ++counter);
+			}
+		);
 		sc.getStylesheets().add("application/application.css");
 		header.getStyleClass().add("header");
 		window.setAlignment(Pos.TOP_CENTER); 
 		window.getChildren().add(header);
+		window.getChildren().add(pressMe);
+		window.getChildren().add(score);
 		primaryStage.setScene(sc);
 		primaryStage.show();
 		
