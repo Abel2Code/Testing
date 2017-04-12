7176cd7da76c48519fe37efe8f2e794115b10f57
Abel2Code <asalinaswc1@aol.com>
2017-04-12 10:21:10 -0700

    Changed Logic

--- Java/JavaFx/AFK Game/Main.java
+++ Java/JavaFx/AFK Game/Main.java
@@ -1,16 +1,19 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	private int counter = 0;
+	private int changeScore;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -18,19 +21,24 @@ public class Main extends Application {
 		VBox window = new VBox();
 		Label header = new Label("Welcome to this AFK game!");
 		Scene sc = new Scene(window, 300, 300);
-		Label score = new Label("Score: " + counter);
+		Label score = new Label("Score: ");
+		Label scoreValue = new Label("0");
 		// int counter = 0;
 		Button pressMe = new Button("Press Me!");
-		pressMe.setOnMouseClicked(e ->{
-			score.setText("Score: " + ++counter);
+		pressMe.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>() {
+			@Override
+			public void handle(Event event){
+				changeScore++;
+				scoreValue.setText(String.valueOf(changeScore));
 			}
-		);
+		});
 		sc.getStylesheets().add("application/application.css");
 		header.getStyleClass().add("header");
 		window.setAlignment(Pos.TOP_CENTER); 
 		window.getChildren().add(header);
 		window.getChildren().add(pressMe);
 		window.getChildren().add(score);
+		window.getChildren().add(scoreValue);
 		primaryStage.setScene(sc);
 		primaryStage.show();
 		
